3d8cdb38efe1b75ce15f9992be0ad17a646b12c2
Manuelpg05 <manuel.p.g.21.24@gmail.com>
2025-05-02 13:18:02 +0200

    Build: Estructuración del proyecto backend

---
+++ Backend-SpringBoot/src/main/java/FrenzyFilms/FrenzyFilmsApplication.java
@@ -0,0 +1,18 @@
+package FrenzyFilms;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenzyFilmsApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrenzyFilmsApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("APLICACIÓN INICIADA CON ÉXITO");
+    }
+}
---
+++ Backend-SpringBoot/src/test/java/FrenzyFilms/FrenzyFilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package FrenzyFilms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrenzyFilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
